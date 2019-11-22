f6791a58a3ac5244063c589c3a587bfa8842ccac
Benja1972 <benja@bk.ru>
2019-11-22 11:06:58 +0100

    new code

---
+++ 5-Text-generation.py
@@ -0,0 +1,62 @@
+import string
+from os.path import join 
+import os
+import pickle
+
+import pandas as pd
+
+DATA = 'transcripts'
+
+data = pd.read_pickle(join(DATA,"clean.pkl"))
+
+# One text only for test
+bowt_text = data.text.loc['Bowt']
+
+
+#~ Build a Markov Chain Function
+from collections import defaultdict
+
+def markov_chain(text):
+    '''The input is a string of text and the output will be a dictionary with each word as
+       a key and each value as the list of words that come after the key in the text.'''
+    
+    # Tokenize the text by word, though including punctuation
+    words = text.split(' ')
+    
+    # Initialize a default dictionary to hold all of the words and next words
+    m_dict = defaultdict(list)
+    
+    # Create a zipped list of all of the word pairs and put them in word: list of next words format
+    for current_word, next_word in zip(words[0:-1], words[1:]):
+        m_dict[current_word].append(next_word)
+
+    # Convert the default dict back into a dictionary
+    m_dict = dict(m_dict)
+    return m_dict
+
+bowt_chain = markov_chain(bowt_text)
+
+
+#~ Create a Text Generator
+
+import random
+
+def generate_sentence(chain, count=15):
+    '''Input a dictionary in the format of key = current word, value = list of next words
+       along with the number of words you would like to see in your generated sentence.'''
+
+    # Capitalize the first word
+    word1 = random.choice(list(chain.keys()))
+    sentence = word1.capitalize()
+
+    # Generate the second word from the value list. Set the new word as the first word. Repeat.
+    for i in range(count-1):
+        word2 = random.choice(chain[word1])
+        word1 = word2
+        sentence += ' ' + word2
+
+    # End it with a period
+    sentence += '.'
+    return(sentence)
+
+sent_bowt = generate_sentence(bowt_chain, count=20)
