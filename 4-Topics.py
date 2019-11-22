08c1ce88333d129177186dab5aa73763ca0322d6
Benja1972 <benja@bk.ru>
2019-11-22 10:08:23 +0100

    refresh code

--- 4-Topics.py
+++ 4-Topics.py
@@ -1,7 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
 import string
 from os.path import join 
 import os
@@ -11,42 +7,10 @@ import pandas as pd
 
 DATA = 'transcripts'
 
-
-
-data = pd.read_pickle(join(DATA,"dtm_stop.pkl"))
-
-from gensim import matutils, models
-import scipy.sparse
-tdm = data.transpose()
-
-
-# We're going to put the term-document matrix into a new gensim format, 
-# from df --> sparse matrix --> gensim corpus
-sparse_counts = scipy.sparse.csr_matrix(tdm)
-corpus = matutils.Sparse2Corpus(sparse_counts)
-
-
-
-# Gensim also requires dictionary of the all terms and their respective 
-# location in the term-document matrix
-cv = pickle.load(open(join(DATA,"cv_stop.pkl"), "rb"))
-id2word = dict((v, k) for k, v in cv.vocabulary_.items())
-
-# Now that we have the corpus (term-document matrix) and id2word (dictionary of location: term),
-# we need to specify two other parameters as well - the number of topics and the number of passes
-lda = models.LdaModel(corpus=corpus, id2word=id2word, num_topics=4, passes=10)
-lda.print_topics()
-
 from nltk import word_tokenize, pos_tag
 
-def nouns(text):
-    '''Given a string of text, tokenize the text and pull out only the nouns.'''
-    is_noun = lambda pos: pos[:2] == 'S'
-    tokenized = word_tokenize(text)
-    all_nouns = [word for (word, pos) in pos_tag(tokenized, lang='rus') if is_noun(pos)] 
-    return ' '.join(all_nouns)
 
-def nouns(text):
+def nouns_adj(text):
     '''Given a string of text, tokenize the text and pull out only the nouns.'''
     is_noun = lambda pos: pos[:2] == 'S' or  pos[:2] == 'A'
     tokenized = word_tokenize(text)
@@ -54,36 +18,44 @@ def nouns(text):
     return ' '.join(all_nouns)
 
 
-
+# Clean text to keep only nouns and adjectives
 data_clean = pd.read_pickle(join(DATA,"clean.pkl"))
+data_nouns = pd.DataFrame(data_clean.text.apply(nouns_adj))
 
-data_nouns = pd.DataFrame(data_clean.text.apply(nouns))
 
-
-## Document-Term Matrix
+## Rebuild Document-Term Matrix
 from nltk.corpus import stopwords
-russian_stopwords = stopwords.words("russian")
-
 from sklearn.feature_extraction.text import CountVectorizer
 
-
-cvn = CountVectorizer(stop_words=russian_stopwords)
-data_cvn = cvn.fit_transform(data_clean.text)
-
-data_dtmn = pd.DataFrame(data_cvn.toarray(), columns=cvn.get_feature_names())
-
-data_dtmn.index = data_clean.index
+russian_stopwords = stopwords.words("russian")
 
 
+cv = CountVectorizer(stop_words=russian_stopwords)
+data_cv = cv.fit_transform(data_clean.text)
 
-# Create the gensim corpus
-corpusn = matutils.Sparse2Corpus(scipy.sparse.csr_matrix(data_dtmn.transpose()))
+data_dtm = pd.DataFrame(data_cv.toarray(), columns=cv.get_feature_names())
+data_dtm.index = data_clean.index
+
+from gensim import matutils, models
+import scipy.sparse
+
+# We're going to put the term-document matrix into a new gensim format, 
+# from df --> sparse matrix --> gensim corpus
+corpus = matutils.Sparse2Corpus(scipy.sparse.csr_matrix(data_dtm.transpose()))
 
 # Create the vocabulary dictionary
-id2wordn = dict((v, k) for k, v in cvn.vocabulary_.items())
+id2word = dict((v, k) for k, v in cv.vocabulary_.items())
 
 
 
 # Let's try 4 topics
-ldan = models.LdaModel(corpus=corpusn, num_topics=4, id2word=id2wordn, passes=10)
-ldan.print_topics()
+lda = models.LdaModel(corpus=corpus, num_topics=4, id2word=id2word, passes=80)
+print('Topics = ', lda.print_topics())
+
+
+#Let's take a look at which topics each transcript contains
+corpus_transformed = lda[corpus]
+
+top_list = list(zip([a for [(a,b)] in corpus_transformed], data_dtm.index))
+
+print('Topics by authors = ', top_list)
